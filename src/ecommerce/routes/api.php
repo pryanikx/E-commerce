b6dbc579259039faf2009e8bdbe47c3050b4230e
pryanikx <ivanmemnikov@gmail.com>
2025-04-24 04:36:18 +0300

    added 'show one product' route to admin's routes group

--- src/ecommerce/routes/api.php
+++ src/ecommerce/routes/api.php
@@ -12,10 +12,15 @@ Route::get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum', 'role:user'])->group(function () {
     Route::get('/products', [ProductController::class, 'index']);
     Route::get('/products/{id}', [ProductController::class, 'show']);
+
+    // Add 'update product' route
 });
 
 Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
     Route::get('/products', [AdminProductController::class, 'index']);
+    Route::get('/products/{id}', [AdminProductController::class, 'show']);
     Route::post('/products', [AdminProductController::class, 'store']);
     Route::delete('/products/{id}', [AdminProductController::class, 'deleteProduct']);
+    
+    // Add 'update product' route
 });
