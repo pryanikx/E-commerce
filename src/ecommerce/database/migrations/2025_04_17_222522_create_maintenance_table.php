b2ff995d677394d11013c5d2808f5f5ad403ed14
pryanikx <ivanmemnikov@gmail.com>
2025-05-04 21:34:46 +0300

    deleted orders table + added 'duration' column to maintenances table

--- src/ecommerce/database/migrations/2025_04_17_222522_create_maintenance_table.php
+++ src/ecommerce/database/migrations/2025_04_17_222522_create_maintenance_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', length: 20)->unique();
             $table->text('description')->nullable();
+            $table->char('duration', 50);
             $table->timestamps();
         });
     }
