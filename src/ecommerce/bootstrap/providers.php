a9584d32f01998d66468104e923ff5d5ce1c0a76
pryanikx <ivanmemnikov@gmail.com>
2025-07-04 12:28:33 +0300

    Initial empty commit for review

--- src/ecommerce/bootstrap/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\CatalogExportServiceProvider::class,
-    App\Providers\CurrencyServiceProvider::class,
-    App\Providers\FilterServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-];
