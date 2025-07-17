c2c4ee6689bc2b1cf2071efddfc82544e8f5d5bd
pryanikx <ivanmemnikov@gmail.com>
2025-07-17 13:15:46 +0300

    Stan + PSR for Jobs

--- src/ecommerce/app/Jobs/ExportCatalogJob.php
+++ src/ecommerce/app/Jobs/ExportCatalogJob.php
@@ -68,19 +68,19 @@ class ExportCatalogJob implements ShouldQueue
                 self::CONTEXT_ADMIN_EMAIL => $this->adminEmail
             ]);
 
-            $csvFilePath = $this->generateCsvFile($exportService, $logger);
-            $storageKey = $this->uploadToStorage($uploader, $csvFilePath, $logger);
+            $csvFilePath = $this->generateCsvFile($exportService);
+            $storageKey = $this->uploadToStorage($uploader, $csvFilePath);
             $stats = $exportService->getExportStats();
 
-            $this->sendSuccessNotification($emailService, $storageKey, $stats, $logger);
+            $this->sendSuccessNotification($emailService, $storageKey, $stats);
 
-            $logger->info(__('messages.export_completed'), [
+            $this->logger->info(__('messages.export_completed'), [
                 self::CONTEXT_EXPORT_ID => $this->exportId,
                 self::CONTEXT_S3_KEY => $storageKey,
                 self::CONTEXT_STATS => $stats
             ]);
         } catch (\Throwable $exception) {
-            $this->handleExportFailure($emailService, $exception, $this->logger);
+            $this->handleExportFailure($emailService, $exception);
 
             throw $exception;
         }
@@ -107,14 +107,12 @@ class ExportCatalogJob implements ShouldQueue
      * Generate CSV file from catalog data.
      *
      * @param ProductExportService $exportService
-     * @param LoggerInterface $logger
      *
      * @return string
      * @throws \Exception
      */
     private function generateCsvFile(
         ProductExportService $exportService,
-        LoggerInterface $logger
     ): string {
         $csvFilePath = $exportService->exportToCSV($this->exportId);
 
@@ -122,7 +120,7 @@ class ExportCatalogJob implements ShouldQueue
             throw new \Exception(__('errors.csv_not_created') . ": {$csvFilePath}");
         }
 
-        $logger->info(__('messages.csv_file_generated'), [
+        $this->logger->info(__('messages.csv_file_generated'), [
             self::CONTEXT_EXPORT_ID => $this->exportId,
             self::CONTEXT_FILE_PATH => $csvFilePath,
             self::CONTEXT_FILE_SIZE => filesize($csvFilePath)
@@ -136,7 +134,6 @@ class ExportCatalogJob implements ShouldQueue
      *
      * @param StorageUploaderInterface $uploader
      * @param string $csvFilePath
-     * @param LoggerInterface $logger
      *
      * @return string
      * @throws \Exception
@@ -144,7 +141,6 @@ class ExportCatalogJob implements ShouldQueue
     private function uploadToStorage(
         StorageUploaderInterface $uploader,
         string $csvFilePath,
-        LoggerInterface $logger
     ): string {
         $storageKey = $uploader->uploadCatalogExport($csvFilePath, $this->exportId);
 
@@ -152,7 +148,7 @@ class ExportCatalogJob implements ShouldQueue
             throw new \Exception(__('errors.storage_upload_failed'));
         }
 
-        $logger->info(__('messages.file_uploaded_to_storage'), [
+        $this->logger->info(__('messages.file_uploaded_to_storage'), [
             self::CONTEXT_EXPORT_ID => $this->exportId,
             self::CONTEXT_S3_KEY => $storageKey
         ]);
@@ -165,8 +161,7 @@ class ExportCatalogJob implements ShouldQueue
      *
      * @param EmailNotificationService $emailService
      * @param string $storageKey
-     * @param array $stats
-     * @param LoggerInterface $logger
+     * @param array<string, mixed> $stats
      *
      * @return void
      * @throws \Throwable
@@ -175,7 +170,6 @@ class ExportCatalogJob implements ShouldQueue
         EmailNotificationService $emailService,
         string $storageKey,
         array $stats,
-        LoggerInterface $logger
     ): void {
         $emailService->sendExportSuccessNotification(
             $this->adminEmail,
@@ -184,7 +178,7 @@ class ExportCatalogJob implements ShouldQueue
             $stats
         );
 
-        $logger->info(__('messages.success_notification_sent'), [
+        $this->logger->info(__('messages.success_notification_sent'), [
             self::CONTEXT_EXPORT_ID => $this->exportId,
             self::CONTEXT_ADMIN_EMAIL => $this->adminEmail
         ]);
@@ -195,16 +189,14 @@ class ExportCatalogJob implements ShouldQueue
      *
      * @param EmailNotificationService $emailService
      * @param \Throwable $exception
-     * @param LoggerInterface $logger
      *
      * @return void
      */
     private function handleExportFailure(
         EmailNotificationService $emailService,
         \Throwable $exception,
-        LoggerInterface $logger
     ): void {
-        $logger->error(__('messages.export_failed'), [
+        $this->logger->error(__('messages.export_failed'), [
             self::CONTEXT_EXPORT_ID => $this->exportId,
             self::CONTEXT_ERROR => $exception->getMessage(),
             self::CONTEXT_TRACE => $exception->getTraceAsString()
@@ -217,12 +209,12 @@ class ExportCatalogJob implements ShouldQueue
                 $exception->getMessage()
             );
 
-            $logger->info(__('messages.failure_notification_sent'), [
+            $this->logger->info(__('messages.failure_notification_sent'), [
                 self::CONTEXT_EXPORT_ID => $this->exportId,
                 self::CONTEXT_ADMIN_EMAIL => $this->adminEmail
             ]);
         } catch (\Throwable $emailException) {
-            $logger->error(__('messages.failure_notification_failed'), [
+            $this->logger->error(__('messages.failure_notification_failed'), [
                 self::CONTEXT_EXPORT_ID => $this->exportId,
                 self::CONTEXT_EMAIL_ERROR => $emailException->getMessage()
             ]);
